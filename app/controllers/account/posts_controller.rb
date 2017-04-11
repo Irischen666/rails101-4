ff9f8020006feee7177ad210fdda4d85fe9ed736
Irischen666 <chenxiyuan8866@gmail.com>
2017-04-11 15:05:49 +0800

    implement edit and delete posts

--- app/controllers/account/posts_controller.rb
+++ app/controllers/account/posts_controller.rb
@@ -3,4 +3,15 @@ class Account::PostsController < ApplicationController
   def index
     @posts = current_user.posts
   end
+
+  def destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to account_posts_path, alert:'Group Deleted'
+  end
+
+  private
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,7 @@ Rails.application.routes.draw do
     resources :posts
   end
 
+  resources :posts
   root 'groups#index'
+  
 end
